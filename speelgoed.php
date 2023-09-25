5c7f0456dca787ed4e43749bb8d487220348ed6d
The_Boy <145575962+TheBoy2901@users.noreply.github.com>
2023-09-25 15:31:39 +0200

    Speelgoed.php geüpload
    Een test om de active's te bekijken

---
+++ speelgoed.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link rel="stylesheet" href="stijl.css">
+
+<ul>
+<li><a href="index.php">Home</a></li>
+  <li><a class="active" href="speelgoed.php">Speelgoed</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+</ul>
+
+</head>
+
+</html>
