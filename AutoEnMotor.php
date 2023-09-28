01b6bfe35c8b91ef963f886450de02bf5e6661b9
The_Boy <145575962+TheBoy2901@users.noreply.github.com>
2023-09-28 13:06:38 +0200

    Add files via upload

---
+++ AutoEnMotor.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="stijl.css">
+
+<ul>
+
+<li><a href="index.php">Home</a></li>
+
+  <li class="dropdown">
+  <a href="javascript:void(0)" class="active" class="dropbtn2">Catogoriën</a>
+    <div class="dropdown-content">
+      <a href="AutoEnMotor.php">Auto en Motor</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+
+</ul>
+
+</head>
+<body>
+<h>Bol.com catogoriën</h>
+
+<title>Bol.com</title>
+<link rel="icon" type="image/x-icon" href="/plaatjes/scrum.JPG">
+</body>
+</html>
