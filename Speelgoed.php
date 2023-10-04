6bc4f8a69eecabcfff720a0b48a5b5345be526cb
The_Boy <145575962+TheBoy2901@users.noreply.github.com>
2023-10-04 15:12:06 +0200

    Nieuwe shit geupload

---
+++ Speelgoed.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="stijl.css">
+
+<ul>
+
+<li><a href="index.php">Home</a></li>
+
+  <li class="dropdown">
+  <a href="javascript:void(0)" class="active" class="dropbtn2">Catogoriën</a>
+    <div class="dropdown-content">
+      <a href="AutoEnMotor.php">Auto en Motor</a>
+      <a href="Computers.php">Computers</a>
+      <a href="ELektronica.php">Elektronica</a>
+      <a href="Games.php">Games</a>
+      <a href="Speelgoed.php">Speelgoed</a>
+      <a href="Verzorging.php">Verzorging</a>
+    </div>
+  </li>
+
+  <li style="float: right" class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Klantenservice</a>
+    <div class="dropdown-content">
+      <a href="#">Bestelstatus</a>
+      <a href="#">Bedrijfsgegevens</a>
+    </div>
+  </li>
+
+</ul>
+
+</head>
+<body>
+<h>Speelgoed</h>
+
+<title>Bol.com</title>
+<link rel="icon" type="image/x-icon" href="/plaatjes/scrum.JPG">
+</body>
+</html>
