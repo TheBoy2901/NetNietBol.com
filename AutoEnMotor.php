752822e9252acc9b2085608cebbec6d9f297188d
The_Boy <145575962+TheBoy2901@users.noreply.github.com>
2023-10-11 15:38:19 +0200

    Update AutoEnMotor.php

--- AutoEnMotor.php
+++ AutoEnMotor.php
@@ -5,27 +5,9 @@
 
 <ul>
 
-<li><a href="index.php">Home</a></li>
-
-  <li class="dropdown">
-  <a href="javascript:void(0)" class="active" class="dropbtn2">Catogoriën</a>
-   <div class="dropdown-content">
-      <a href="AutoEnMotor.php">Auto en Motor</a>
-      <a href="Computers.php">Computers</a>
-      <a href="Elektronica.php">Elektronica</a>
-      <a href="Games.php">Games</a>
-      <a href="Speelgoed.php">Speelgoed</a>
-      <a href="Verzorging.php">Verzorging</a>
-    </div>
-  </li>
-
-  <li style="float: right" class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Klantenservice</a>
-    <div class="dropdown-content">
-      <a href="#">Bestelstatus</a>
-      <a href="#">Bedrijfsgegevens</a>
-    </div>
-  </li>
+<?php
+require_once(navbar);
+?>
 
 </ul>
 
