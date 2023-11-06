fe81e437078f0dd2a2f4f53bb05dadc97a79a4db
2leaped <145574798+2leaped@users.noreply.github.com>
2023-11-06 15:41:44 +0100

    Update Computers.php

--- Computers.php
+++ Computers.php
@@ -6,14 +6,41 @@
 <ul>
 
 <?php
-require_once(navbar);
+require_once("navbar.php");
 ?>
 
 </ul>
 
 </head>
 <body>
-<h>Computers</h>
+
+
+<table style="width:100%">
+
+<html>
+<head>
+<style>
+
+h1 {
+  color: red;
+}
+h3 {
+  color: black;
+}
+
+td {
+  text-align: center;
+}
+
+</style>
+</head>
+
+  <tr>
+   <td><a href="bestel.php"><img src="ExamplePlaatjes/macbook.png" width="400" height="333"></td>
+    <td><b>Apple Macbook Pro</b></td>
+    <td><h1>2629</h1></td>
+  </tr>
+
 
 <title>Bol.com</title>
 <link rel="icon" type="image/x-icon" href="/plaatjes/scrum.JPG">
