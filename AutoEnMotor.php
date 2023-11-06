d78b6342768334c180415f9dd0b822fc295a91fa
2leaped <145574798+2leaped@users.noreply.github.com>
2023-11-06 15:42:08 +0100

    Update AutoEnMotor.php

--- AutoEnMotor.php
+++ AutoEnMotor.php
@@ -1,12 +1,11 @@
-<!doctype html>
 <html>
 <head>
 <link rel="stylesheet" href="stijl.css">
 
 <ul>
-
+ 
 <?php
-require_once(navbar);
+require_once("navbar.php");
 ?>
 
 </ul>
@@ -14,34 +13,90 @@ require_once(navbar);
 </head>
 <body>
 
-<html>
-<style>
-table, th, td {
-  border:1px solid black;
-}
-</style>
-<body>
-
 <table style="width:100%">
-  <tr>
-    <th>Example</th>
-    <th>Product</th>
-    <th>Price</th>
-  </tr>
-  <tr>
-    <td></td>
-    <td>Parkeerschijf</td>
-    <td>$1.25</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td>Telefoonhouder</td>
-    <td>$19.99</td>
-  </tr>
-</table>
 
-</body>
-</html>
+<head>
+<style>
+
+h1 {
+  color: red;
+}
+
+td {
+  text-align: center;
+}
+
+</style>
+</head>
+
+  <tr>
+    <td><a href="bestel.php?product=parkeerschijf&prijs=1,25&plaatje=parkeerschijf.jpg"><img src="ExamplePlaatjes/parkeerschijf.jpg" width="500" height="333"></a></td>
+    <td><b>Parkeerschijf</b></td>
+    <td><h1>1,25</h1></td>
+  </tr>
+
+  <tr>
+    <td><a href="bestel.php?product=Telefoonhouder&prijs=19,99&plaatje=telefoonhouder.jpg"><img src="ExamplePlaatjes/telefoonhouder.png" width="400" height="333"></a></td>
+    <td><b>Telefoonhouder<b></td>
+    <td><h1>19,99</h1></td>
+  </tr>
+
+  <tr>
+    <td><img src="ExamplePlaatjes/autogripmat.png" width="400" height="333"></td>
+    <td><b>Auto gripmat<b></td>
+    <td><h1>50,95</h1></td>
+  </tr>
+
+  <tr>
+    <td><img src="ExamplePlaatjes/koelvloeistof.png" width="300" height="333"></td>
+    <td><b>Protection koelvloeistof</b></td>
+    <td><h1>19,99</h1></td>
+  </tr>
+
+
+  <tr>
+    <td><img src="ExamplePlaatjes/wd40.png" width="400" height="333"></td>
+     <td><b>WD-40</b></td>
+    <td><h1>14,99</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/motorslot.png" width="400" height="333"></td>
+     <td><b>Motorslot</b></td>
+    <td><h1>38,95</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/scooterhelm.png" width="400" height="333"></td>
+     <td><b>Scooterhelm</b></td>
+    <td><h1>54,95</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/bagagedragerfiets.png" width="400" height="333"></td>
+     <td><b>Bagagedrager Fiets</b></td>
+    <td><h1>24,95</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/electrischestep.png" width="300" height="400"></td>
+     <td><b>Elektrische Step</b></td>
+    <td><h1>270,-</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/fatbike.png" width="400" height="350"></td>
+     <td><b>Fatbike</b></td>
+    <td><h1>1299,-</h1></td>
+  </tr>
+  
+  <tr>
+    <td><img src="ExamplePlaatjes/fietspomp.png" width="400" height="333"></td>
+     <td><b>Fietspomp</b></td>
+    <td><h1>19,89</h1></td>
+  </tr>
+  
+</table>
 
 <title>Bol.com</title>
 <link rel="icon" type="image/x-icon" href="/plaatjes/scrum.JPG">
