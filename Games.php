b2a636770f19ece958212823f5f17804cf4e0794
2leaped <145574798+2leaped@users.noreply.github.com>
2023-11-06 15:40:53 +0100

    Update Games.php

--- Games.php
+++ Games.php
@@ -6,14 +6,49 @@
 <ul>
 
 <?php
-require_once(navbar);
+require_once("navbar.php");
 ?>
 
 </ul>
 
 </head>
+
+<table style="width:100%">
+
 <body>
-<h>Games</h>
+<head>
+<style>
+
+h1 {
+  color: red;
+}
+h3 {
+  color: black;
+}
+
+td {
+  text-align: center;
+}
+
+</style>
+</head>
+
+  <tr>
+   <td><a href="bestel.php"><img src="ExamplePlaatjes/fc24.png" width="380" height="433"></td>
+    <td><b>EAFC 24 PS5</b></td>
+    <td><h1>64,99</h1></td>
+  </tr>
+ <tr>
+   <td><a href="bestel.php"><img src="ExamplePlaatjes/motorfest.png" width="380" height="433"></td>
+    <td><b>The crew motorfest PS5</b></td>
+    <td><h1>64,99</h1></td>
+  </tr>
+<tr>
+   <td><a href="bestel.php"><img src="ExamplePlaatjes/goatsim3.png" width="380" height="433"></td>
+    <td><b>Goat Simulator 3 PS5</b></td>
+    <td><h1>19,99</h1></td>
+  </tr>
+
 
 <title>Bol.com</title>
 <link rel="icon" type="image/x-icon" href="/plaatjes/scrum.JPG">
